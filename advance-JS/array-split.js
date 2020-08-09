e3744dfd94a322c4fc35a2ee972e58035a91252f
shakilmahammud <hshakil987@gmail.com>
2020-08-09 10:20:47 +0600

    array slpit

---
+++ advance-JS/array-split.js
@@ -0,0 +1,9 @@
+const numbers=[3,4,5,6,7,8,10,15];
+const result=numbers.slice(2,5);
+console.log(result);
+
+const result2=numbers.splice(2,5);
+console.log(result2);
+
+const result3=numbers.join(" ");
+console.log(result3);
